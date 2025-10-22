86a06b77fec37b1fe1f05d0598bffb33766dabc9
joe1962 <joe1962@gmail.com>
2025-10-22 15:33:24 -0400

    Updated LibInfo, CHANGELOG for real now. Signed-off-by:joe1962 <joe1962@gmail.com>

--- src/main/java/cu/jsoft/j_dbfx/LibInfo.java
+++ src/main/java/cu/jsoft/j_dbfx/LibInfo.java
@@ -11,8 +11,8 @@ package cu.jsoft.j_dbfx;
 public class LibInfo {
 	// Lib constants:
 	private static final String TITLE = "j_dbfx";
-	private static final String VERSION = "1.0.8";
-	private static final String BUILD = "251022.02";
+	private static final String VERSION = "1.0.9";
+	private static final String BUILD = "251022.03";
 
 
 
